93c7f56c81bd7eab5977230f5722018409255701
YanLien <YJQ980314@outlook.com>
2024-12-03 14:50:06 +0800

    hello_app

--- build_img.sh
+++ build_img.sh
@@ -1,22 +1,15 @@
-# ARCH=riscv64gc-unknown-none-elf
-# APP=hello_app
-
-# cargo build --target $ARCH --release
-
-# rust-objcopy --binary-architecture=riscv64 --strip-all -O binary target/$ARCH/release/$APP ./$APP.bin
-
-# dd if=/dev/zero of=./apps.bin bs=1M count=32
-# dd if=./hello_app.bin of=./apps.bin conv=notrunc
-
-# mkdir -p ../arceos/payload
-# mv ./apps.bin ../arceos/payload/apps.bin
-
 cargo build --target riscv64gc-unknown-none-elf --release
 
 rust-objcopy --binary-architecture=riscv64 --strip-all -O binary target/riscv64gc-unknown-none-elf/release/hello_app ./hello_app.bin
 
+size=$(xxd -p ./hello_app.bin | tr -d '\n' | wc -c)
+echo "Size: $((size/2))"
+
 dd if=/dev/zero of=./apps.bin bs=1M count=32
-dd if=./hello_app.bin of=./apps.bin conv=notrunc
+printf "%02x" $((size/2)) | xxd -p -r | dd of=apps.bin conv=notrunc
+dd if=./hello_app.bin of=./apps.bin seek=1 bs=1 conv=notrunc 
+
+xxd -l 10 -p ./apps.bin
 
 mkdir -p ../arceos/payload
 mv ./apps.bin ../arceos/payload/apps.bin
